ad3d14ec620d09e6930439c6c34467490a662db3
Parth Agrawal <parth.agrawal@outlook.com>
2019-12-22 17:34:06 -0500

    Added plaintext percentage handling for input

--- bayes.py
+++ bayes.py
@@ -1,9 +1,23 @@
+def trimmed_input():
+    input_str = input(">")
+    output = ""
+    for x in input_str:
+        if(x.isnumeric() or x == "."):
+            output += str(x)
+        else:
+            return output
+            
+
+
 print("Enter prior probability, probability that the hypothesis is true")
-hyp_t = float(input(">"))
+
+hyp_t = float(trimmed_input())/100
+
+
 print("Enter likelihood of experiment, given hypothesis true")
-like_hyp_t = float(input(">"))
+like_hyp_t = float(trimmed_input())/100
 print("Enter likelihood of experiment, given hypothesis false")
-like_hyp_f = float(input(">"))
+like_hyp_f = float(trimmed_input())/100
 
 # compare the weightage of the two
 hyp_f = 1-hyp_t
